89419696c64697e7767d018919b6785f5e09a229
Amy Lindley <amyklindley@live.com>
2019-05-22 15:52:43 -0500

    First commit

---
+++ RGPGameApp/Classes/StatLine.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RGPGameApp.Classes
+{
+    public class StatLine
+    {
+        private Random rnd = new Random();
+        private int[] StatRoll = new int[4];
+        private int[] LineRoll = new int[6];
+        
+        public StatLine()
+        {
+            RollStatLine();
+        }
+        private int OneDice()
+        {
+            return rnd.Next(6) + 1;
+        }
+
+        private int StatDiceRoll()
+        {
+            int result;
+            for (int i = 0; i < StatRoll.Length; i++)
+            {
+                StatRoll[i] = OneDice();
+            }
+            
+            for (int i = 0; i < StatRoll.Length; i++)
+            {
+                if (StatRoll[i] == 1)
+                    StatRoll[i] = OneDice();
+                
+            }
+
+            Array.Sort(StatRoll);
+            Array.Reverse(StatRoll);
+
+            result = StatRoll[0] + StatRoll[1] + StatRoll[2];
+
+            return result;
+        }
+
+        public int[] RollStatLine()
+        {
+            for (int i = 0; i < LineRoll.Length; i++)
+            {
+                LineRoll[i] = StatDiceRoll();
+            }
+
+            return LineRoll;
+        }
+    }
+}
+
--- RGPGameApp/Pages/Games.cshtml.cs
+++ RGPGameApp/Pages/Games.cshtml.cs
@@ -4,17 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using RGPGameApp.Classes;
 
 namespace RGPGameApp.Pages
 {
-    public class IndexModel : PageModel
+    public class GamesModel : PageModel
     {
         public void OnGet()
         {
-            StatLine fLine = new StatLine();
-            ViewData["statLine1"] = fLine.ToString();
-            //ViewData["statLine2"] = new StatLine();
+
         }
     }
 }
